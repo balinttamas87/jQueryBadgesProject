a0d8c376ebf8aa8eb14f8279e143832d1aa7b9e1
Tamas Balint <balint.tamas87@gmail.com>
2017-08-05 16:59:33 +0100

    add one div element per completed course

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,7 +4,10 @@ $(function() {
 	url: "https://www.codeschool.com/users/balinttamas87.json",
 	dataType: "jsonp",
 	success: function(response) {
-
+		var completedCoursesArr = response.courses.completed;
+		for (var i = 0; i < completedCoursesArr.length; i++) {
+			$("#badges").append("<div class='course'></div>");
+		}
 	}
   });
 
