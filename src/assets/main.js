9c0bc4608e36f12308f4e67a2abc5e8257c1ca69
Tamas Balint <balint.tamas87@gmail.com>
2017-08-05 17:42:56 +0100

    add course buttons

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,14 +7,16 @@ $(function() {
 		var completedCoursesArr = response.courses.completed;
 		var currentCompletedCourseTitle = "";
 		var sourceOfCurrentBadgeImage = "";
+		var currentCourseUrl = "";
 		for (var i = 0; i < completedCoursesArr.length; i++) {
 			currentCompletedCourseTitle = completedCoursesArr[i].title;
 			sourceOfCurrentBadgeImage = completedCoursesArr[i].badge;
+			currentCourseUrl = completedCoursesArr[i].url;
 			$("#badges").append("<div class='course'>" + 
 									"<h3>" + currentCompletedCourseTitle + "</h3>" + 
 									"<img src=" + sourceOfCurrentBadgeImage + " alt='badgeImage'/>" +
+									"<a class='btn btn-primary' href=" + currentCourseUrl +  '"' + " target='_blank'>See Course</a>" +
 								"</div>");
-
 		}
 	}
   });
